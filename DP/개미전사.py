b388f69b6b72b9ffd265e05118c39e3adf66ed14
c0c0pang <coc1789@naver.com>
2023-01-24 00:08:20 +0900

    개미전사 풀이

---
+++ DP/개미전사.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+array = list(map(int,sys.stdin.readline().rstrip().split()))
+
+dp = [0]*n
+dp[0]=array[0]
+dp[1]=array[1]
+for i in range(2,n):
+    dp[i] = dp[i-2]+array[i] 
+    dp[i] = max(dp[i],dp[i-1])
+   
+print(dp[n-1])
