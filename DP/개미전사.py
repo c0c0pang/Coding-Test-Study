be22872349934e6cc911f29e4deb4906f88ac69d
c0c0pang <coc1789@naver.com>
2023-01-24 04:05:38 +0900

    개미 전사 풀이 수정

--- DP/개미전사.py
+++ DP/개미전사.py
@@ -6,7 +6,7 @@ array = list(map(int,sys.stdin.readline().rstrip().split()))
 
 dp = [0]*n
 dp[0]=array[0]
-dp[1]=array[1]
+dp[1]=max(array[0],array[1])
 for i in range(2,n):
     dp[i] = dp[i-2]+array[i] 
     dp[i] = max(dp[i],dp[i-1])
