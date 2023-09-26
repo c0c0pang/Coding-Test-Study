24fd420ac80254651427197baa0a6fffd798f650
c0c0pang <coc1789@naver.com>
2023-09-26 11:00:14 +0900

    오답 수정

--- 기타/1107.py
+++ 기타/1107.py
@@ -3,15 +3,15 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 m = int(sys.stdin.readline().rstrip())
 min_num = abs(100-n)
-if m == 0:
-    print(0)
+if m!=0:
+    broken = list(sys.stdin.readline().rstrip().split())
 else:
-    broken = list(map(int,sys.stdin.readline().rstrip().split()))
-    for i in range(1000001):
-        num = str(i)
-        for j in range(len(num)):
-            if int(num[j]) in broken:
-                break
-            elif j == len(num)-1:
-                min_num = min(min_num,abs(int(num)-n)+len(num))
-    print(min_num)  
+    broken = []
+for i in range(1000001):
+    num = str(i)
+    for j in num:
+        if j in broken:
+            break
+    else:
+        min_num = min(min_num,abs(i-n)+len(num))
+print(min_num)  
