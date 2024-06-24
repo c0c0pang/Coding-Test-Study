9db41ee2aff119e9190a2fa19f4040e261d18396
cocopang <91197026+c0c0pang@users.noreply.github.com>
2024-06-24 14:24:30 +0900

    [Silver I] Title: 회의실 배정, Time: 620 ms, Memory: 48240 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -22,24 +22,30 @@ public class Main {
 			array[i]= r;
 		}
 		Arrays.sort(array,(a,b)->{
+			//끝 시간이 같으면 처음 시간이 빠른 순으로 정렬 
 			if(a[1]==b[1]) {
 				return a[0] - b[0];
  			}
+			//끝 시간을 기준으로 오름차순 정렬
 			return a[1] - b[1];
 		});
-		int cy=0;
+		//이전의 위치를 저장하는 변수.
+		int prev=0;
 		for(int i =0;i<array.length;i++) {
+			//초기화 작업
 			if(i==0) {
-				cy = array[i][1];
+				prev= array[i][1];
 				answer++;
 				continue;
 			}
+			//회의시간의 시작과 끝이 같은 경우는 반영이 안되기에 카운팅만 실시.
 			if(array[i][0]==array[i][1]) {
 				answer++;
 				continue;
 			}
-			if(cy<=array[i][0]) {
-				cy = array[i][1];
+			//이전 위치와 현재 위치의 시작점을 비교함.
+			if(prev<=array[i][0]) {
+				prev = array[i][1];
 				answer++;
 				continue;
 			}
