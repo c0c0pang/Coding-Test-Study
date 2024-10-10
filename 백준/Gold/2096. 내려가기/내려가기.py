49dc03b920dd457d9915a86a4ee491622fdfd6cf
cocopang <91197026+c0c0pang@users.noreply.github.com>
2024-10-11 01:03:51 +0900

    [Gold V] Title: 내려가기, Time: 284 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -2,20 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-LEN = 3
-max_num = [0]*LEN
-min_num = [0]*LEN
-for i in range(n):
-    down = list(map(int,input().split()))
-    if i==0:
-        for i in range(LEN):
-            max_num[i] = down[i]
-            min_num[i] = down[i]
-        continue
-    max_result = [max(down[0]+max_num[0],down[0]+max_num[1]), max(down[1]+max_num[0],down[1]+max_num[1],down[1]+max_num[2]), max(down[2]+max_num[1],down[2]+max_num[2])]
-    min_result = [min(down[0]+min_num[0],down[0]+min_num[1]), min(down[1]+min_num[0],down[1]+min_num[1],down[1]+min_num[2]), min(down[2]+min_num[1],down[2]+min_num[2])]
-    for i in range(LEN):
-        max_num[i] = max_result[i]
-        min_num[i] = min_result[i]
-
+arr = list(map(int,input().split()))
+max_num = arr
+min_num = arr
+for i in range(1,n):
+    x,y,z = map(int,input().split())
+    max_num = [max(max_num[0]+x,max_num[1]+x),max(max_num[0]+y,max_num[1]+y,max_num[2]+y),max(max_num[1]+z,max_num[2]+z)]
+    min_num = [min(min_num[0]+x,min_num[1]+x),min(min_num[0]+y,min_num[1]+y,min_num[2]+y),min(min_num[1]+z,min_num[2]+z)]
 print(max(max_num),min(min_num))
